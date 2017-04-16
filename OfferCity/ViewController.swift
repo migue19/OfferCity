83d969e6125871a13c83aeb015ef2f7de08a6925
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2017-04-15 20:10:42 -0500

    Initial Commit

---
+++ OfferCity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OfferCity
+//
+//  Created by Miguel Mexicano Herrera on 15/04/17.
+//  Copyright © 2017 Miguel Mexicano Herrera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
