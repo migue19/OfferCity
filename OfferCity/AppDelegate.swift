3a74dd65741efaa6dde5a24c3666a26408d915fa
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2017-05-17 14:21:59 -0500

    Agregar restricción de session en el AppDelegate

--- OfferCity/AppDelegate.swift
+++ OfferCity/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
+    
     static var persistentContainer: NSPersistentContainer = {
         return (UIApplication.shared.delegate as! AppDelegate).persistentContainer
     }()
@@ -49,6 +50,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // MARK: - Tab Bar tintColor
         UITabBar.appearance().tintColor = UIColor(red: 121.0 / 255, green: 148.0 / 255, blue: 180.0 / 255, alpha: 1)
         
+        
+        let context = AppDelegate.viewContext
+               do{
+                      let settings = try context.fetch(Settings.fetchRequest())
+                        if settings.count > 0 {
+                
+                                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                                let main = storyboard.instantiateViewController(withIdentifier: "TabPrincipal") as! TabBarPrincipalController
+                                self.window!.rootViewController = main
+                            }
+                 }
+             catch{
+                        print("Error al obtener los Datos de la DB-> AppDelegate ")
+                    }
         
         return true
     }
