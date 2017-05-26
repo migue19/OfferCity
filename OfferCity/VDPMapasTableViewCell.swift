54d043c585b22e46e42c63d7ee66022be46174a7
Gmo Ginppian <ginppian@gmail.com>
2017-05-26 00:15:03 -0500

    Corrigieron los mapas de las vista detallada de eventos y promos

--- OfferCity/VDPMapasTableViewCell.swift
+++ OfferCity/VDPMapasTableViewCell.swift
@@ -11,12 +11,20 @@ import GoogleMaps
 
 class VDPMapasTableViewCell: UITableViewCell {
 
+    // MARK: - Outlets
+
     @IBOutlet weak var mapView: GMSMapView!
+    
+    // MARK: - Propertys
+
     static let identifier = "VDPMapasTableViewCell"
     
+    // MARK: - Constructor
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        loadView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -25,11 +33,29 @@ class VDPMapasTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
-    
+    // MARK: - Actions
+
     @IBAction func buttonAction(_ sender: UIButton) {
         print("Mapa 📌")
     }
     
+}
+
+extension VDPMapasTableViewCell {
     
-    
+    func loadView() {
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 19.071514,
+                                              longitude: -98.245873,
+                                              zoom: 15.0)
+        
+        mapView.camera = camera
+        mapView.isMyLocationEnabled = true
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 19.071514, longitude: -98.245873)
+        marker.title = "Puebla"
+        marker.snippet = "México"
+        marker.map = mapView
+    }
 }
