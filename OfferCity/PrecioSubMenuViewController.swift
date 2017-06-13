297618b76676e6e032312fb134b35c0116333d6b
Gmo Ginppian <ginppian@gmail.com>
2017-06-13 09:41:48 -0500

    Some

--- OfferCity/PrecioSubMenuViewController.swift
+++ OfferCity/PrecioSubMenuViewController.swift
@@ -16,7 +16,7 @@ class PrecioSubMenuViewController: UIViewController {
     
     // MARK: - Propertys
     
-    let arrayTamano = ["Chico", "Mediano", "Grande"]
+    let arrayTamano = ["Chicoo", "Mediano", "Grande"]
     let arrayPrecio = ["$55.00", "$65.00", "$75.00"]
     
     var tituloTiempo = String()
